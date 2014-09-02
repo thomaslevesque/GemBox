41c68218cda4afd1a993f4b7a29b3256a68f682c
Thomas Levesque <tom103@free.fr>
2014-09-02 02:15:10 +0200

    Added ComparerExtensions and Interval

---
+++ GemBox/ComparerExternsions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GemBox
+{
+    public static class CompareExtensions
+    {
+        public static IComparer<T> ToComparer<T>(this Comparison<T> comparison)
+        {
+            if (comparison == null) throw new ArgumentNullException("comparison");
+            return new ComparisonComparer<T>(comparison);
+        }
+
+        public static Comparison<T> ToComparison<T>(this IComparer<T> comparer)
+        {
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            return comparer.Compare;
+        }
+
+        public static IComparer<T> Reverse<T>(this IComparer<T> comparer)
+        {
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            return new ReverseComparer<T>(comparer);
+        }
+
+        public static Comparison<T> Reverse<T>(this Comparison<T> comparison)
+        {
+            if (comparison == null) throw new ArgumentNullException("comparison");
+            return (x, y) => comparison(y, x);
+        }
+
+        public static T Max<T>(this IComparer<T> comparer, T x, T y)
+        {
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            if (comparer.Compare(x, y) >= 0)
+                return x;
+            return y;
+        }
+
+        public static T Min<T>(this IComparer<T> comparer, T x, T y)
+        {
+            if (comparer == null) throw new ArgumentNullException("comparer");
+            if (comparer.Compare(x, y) <= 0)
+                return x;
+            return y;
+        }
+
+        class ComparisonComparer<T> : Comparer<T>
+        {
+            private readonly Comparison<T> _comparison;
+
+            public ComparisonComparer(Comparison<T> comparison)
+            {
+                _comparison = comparison;
+            }
+
+            public override int Compare(T x, T y)
+            {
+                return _comparison(x, y);
+            }
+        }
+
+        class ReverseComparer<T> : IComparer<T>
+        {
+            private readonly IComparer<T> _baseComparer;
+
+            public ReverseComparer(IComparer<T> baseComparer)
+            {
+                _baseComparer = baseComparer;
+            }
+            
+            #region Implementation of IComparer<T>
+
+            public int Compare(T x, T y)
+            {
+                return _baseComparer.Compare(y, x);
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ GemBox/Interval.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GemBox
+{
+    public class Interval<T>
+    {
+        private readonly T _start;
+        private readonly T _end;
+
+        public Interval(T start, T end)
+        {
+            _start = start;
+            _end = end;
+        }
+
+        public T Start
+        {
+            get { return _start; }
+        }
+
+        public T End
+        {
+            get { return _end; }
+        }
+
+        public bool OverlapsWith(Interval<T> other, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            return this.Contains(other.Start, comparer)
+                || this.Contains(other.End, comparer)
+                || other.Contains(this.Start, comparer)
+                || other.Contains(this.End, comparer);
+        }
+
+        public Interval<T> Union(Interval<T> other, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            T start = comparer.Min(this.Start, other.Start);
+            T end = comparer.Max(this.End, other.End);
+            return Interval.Create(start, end);
+        }
+
+        public Interval<T> Intersection(Interval<T> other, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            T start = comparer.Max(this.Start, other.Start);
+            T end = comparer.Min(this.End, other.End);
+            return Interval.Create(start, end);
+        }
+
+        public bool Contains(T value, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            return comparer.Compare(this.Start, value) <= 0
+                && comparer.Compare(value, this.End) <= 0;
+        }
+    }
+
+    public static class Interval
+    {
+        public static Interval<T> Create<T>(T start, T end)
+        {
+            return new Interval<T>(start, end);
+        }
+    }
+
+    public static class IntervalExtensions
+    {
+        public static IEnumerable<Interval<T>> Consolidate<T>(this IEnumerable<Interval<T>> intervals)
+        {
+            return intervals.Consolidate(null);
+        }
+
+        public static IEnumerable<Interval<T>> Consolidate<T>(this IEnumerable<Interval<T>> intervals, IComparer<T> comparer)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            bool first = true;
+            Interval<T> prev = null;
+            intervals = intervals.OrderBy(i => i.Start, comparer)
+                                 .ThenBy(i => i.End, comparer);
+            foreach (var item in intervals)
+            {
+                if (first)
+                {
+                    prev = item;
+                    first = false;
+                    continue;
+                }
+
+                if (item.OverlapsWith(prev))
+                {
+                    prev = item.Union(prev);
+                }
+                else
+                {
+                    yield return prev;
+                    prev = item;
+                }
+            }
+            if (!first)
+                yield return prev;
+        }
+    }
+}
